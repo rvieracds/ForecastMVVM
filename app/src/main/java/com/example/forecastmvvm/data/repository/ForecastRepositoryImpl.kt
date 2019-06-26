2f5960e3eaf922365d4cf8e24ce5538d81863bed
rvieracds <rviera@codigodelsur.com>
2019-06-26 17:36:24 -0300

    add persitence immediately after fetch data from API

---
+++ app/src/main/java/com/example/forecastmvvm/data/repository/ForecastRepositoryImpl.kt
@@ -0,0 +1,58 @@
+package com.example.forecastmvvm.data.repository
+
+import androidx.lifecycle.LiveData
+import com.example.forecastmvvm.data.db.CurrentWeatherDao
+import com.example.forecastmvvm.data.db.unitlocalized.UnitSpecificCurrentWeatherEntry
+import com.example.forecastmvvm.data.network.WeatherNetworkDataSource
+import com.example.forecastmvvm.data.network.response.CurrentWeatherResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.time.ZonedDateTime
+import java.util.*
+
+class ForecastRepositoryImpl(
+    private val currentWeatherDao: CurrentWeatherDao,
+    private val weatherNetworkDataSource: WeatherNetworkDataSource
+) : ForecastRepository {
+
+    init {
+        weatherNetworkDataSource.downloadedCurrentWeather.observeForever { newCurrentWeather ->
+            persistFetchedCurrentWeather(newCurrentWeather)
+        }
+    }
+
+    override suspend fun getCurrentWeather(metric: Boolean): LiveData<out UnitSpecificCurrentWeatherEntry> {
+        // withContext return values and launch doesnt return value
+        return withContext(Dispatchers.IO) {
+            initWeatherData()
+            return@withContext if (metric) currentWeatherDao.getWeatherMetric()
+            else currentWeatherDao.getWeatherImperial()
+        }
+    }
+
+    private fun persistFetchedCurrentWeather(fetchedWeather: CurrentWeatherResponse) {
+        GlobalScope.launch(Dispatchers.IO) {
+            currentWeatherDao.upsert(fetchedWeather.current)
+        }
+    }
+
+    private suspend fun initWeatherData() {
+        if (isFetchCurrentNeeded(ZonedDateTime.now().minusHours(1)))
+            fetchCurrentWeather()
+    }
+
+    private suspend fun fetchCurrentWeather() {
+        weatherNetworkDataSource.fetchCurrentWeather(
+            "Punta del Este",
+            Locale.getDefault().language
+        )
+
+    }
+
+    private fun isFetchCurrentNeeded(lastFetchTime: ZonedDateTime): Boolean {
+        val thirtyMinutesAgo = ZonedDateTime.now().minusMinutes(30)
+        return lastFetchTime.isBefore(thirtyMinutesAgo)
+    }
+}
