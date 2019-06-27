75ba9580a58380df85a1098fca7d1ea612ae6df7
rvieracds <rviera@codigodelsur.com>
2019-06-27 15:49:12 -0300

    app in progress

--- app/src/main/java/com/example/forecastmvvm/ui/weather/current/CurrentWeatherViewModel.kt
+++ app/src/main/java/com/example/forecastmvvm/ui/weather/current/CurrentWeatherViewModel.kt
@@ -2,14 +2,15 @@ package com.example.forecastmvvm.ui.weather.current
 
 import androidx.lifecycle.ViewModel;
 import com.example.forecastmvvm.data.repository.ForecastRepository
+import com.example.forecastmvvm.internal.UnitSystem
 
 class CurrentWeatherViewModel(
     private val forecastRepository: ForecastRepository
 ) : ViewModel() {
-    private val unitSystem = "metric"
+    private val unitSystem = UnitSystem.METRIC
 
     val isMetric: Boolean
-        get() = true
+        get() = unitSystem == UnitSystem.METRIC
 
-    val weather = forecastRepository.getCurrentWeather()
+    val weather = forecastRepository.getCurrentWeather(isMetric)
 }
